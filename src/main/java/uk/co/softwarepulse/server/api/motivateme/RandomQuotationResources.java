cc03ea44eae9c02ef08c01319e3e75d248c2847f
Imran R <67403229+space-ninja-x@users.noreply.github.com>
2022-01-20 22:13:50 +0530

    Updated RandomQuotationResources.java
    The class now has a method to a return a random quote. For now, the quote isn't random instead it's fixed.

--- src/main/java/uk/co/softwarepulse/server/api/motivateme/RandomQuotationResources.java
+++ src/main/java/uk/co/softwarepulse/server/api/motivateme/RandomQuotationResources.java
@@ -1,5 +1,14 @@
 package uk.co.softwarepulse.server.api.motivateme;
 
-public class RandomQuotationResources {
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
 
+@Path("/quote")
+public class RandomQuotationResources
+{
+    @GET
+    public String showRandomQuote()
+    {
+        return "An A student manages time, an A* student makes time." ;
+    }
 }
