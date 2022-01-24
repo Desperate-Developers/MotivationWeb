d60bd22612f63da4b8aa22b1df30f72f9f4c50d3
Josh James <joshjames.jj2002@gmail.com>
2022-01-24 23:24:33 +0000

    Update IdQuotationResources.java
    - Added web path annotation to class
    - Added showQuoteBasedOnID method along with get annotation
    - Added relevant imports

--- src/main/java/uk/co/softwarepulse/server/api/motivateme/IdQuotationResources.java
+++ src/main/java/uk/co/softwarepulse/server/api/motivateme/IdQuotationResources.java
@@ -1,5 +1,14 @@
 package uk.co.softwarepulse.server.api.motivateme;
 
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.QueryParam;
+
+@Path("/quote")
 public class IdQuotationResources {
 
+    @GET
+    public String showQuoteBasedOnID(@QueryParam("id") int id) {
+        return "A quote is only as good at quote " + id + ".";
+    }
 }
