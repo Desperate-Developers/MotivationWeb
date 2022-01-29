fdf3200759b6502ec5c5f3e2714a07127125a671
imran <imranrizwan1115@gmail.com>
2022-01-29 18:13:11 +0530

    Update Quote.java
    Added the properties, getter and setter methods and the toString method

--- src/main/java/uk/co/softwarepulse/server/api/motivateme/data/Quote.java
+++ src/main/java/uk/co/softwarepulse/server/api/motivateme/data/Quote.java
@@ -2,4 +2,36 @@ package uk.co.softwarepulse.server.api.motivateme.data;
 
 public class Quote {
 
+    private String author ;
+    private String category ;
+    private String quotation ;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getQuotation() {
+        return quotation;
+    }
+
+    public void setQuotation(String quotation) {
+        this.quotation = quotation;
+    }
+
+    @Override
+    public String toString() {
+        return "Author: " + this.author + ", Category: " + this.category + ", Quote: " + this.quotation ;
+    }
 }
