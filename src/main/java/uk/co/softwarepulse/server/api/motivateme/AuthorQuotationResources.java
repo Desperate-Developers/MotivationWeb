754169d689728544d301ce438614e402586669cc
Klaus <Klausce1997@gmail.com>
2022-01-30 23:54:15 -0500

    Method of author created

--- src/main/java/uk/co/softwarepulse/server/api/motivateme/AuthorQuotationResources.java
+++ src/main/java/uk/co/softwarepulse/server/api/motivateme/AuthorQuotationResources.java
@@ -1,5 +1,16 @@
 package uk.co.softwarepulse.server.api.motivateme;
 
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.QueryParam;
+
+
+
+@Path("/quote")
 public class AuthorQuotationResources {
-//testing commit 3333
+
+    @GET
+    public String showQuoteBasedOnAuthor(@QueryParam("author") String author) {
+        return "A quote is only as good as quote " + author + ".";
+    }
 }
