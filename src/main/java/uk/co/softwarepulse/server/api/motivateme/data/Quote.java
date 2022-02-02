34c7f540e62a4e664fa8f8510dae663a7d48d714
John McNeil <john.mcneil@softwarepulse.co.uk>
2022-02-02 08:24:44 +0000

    Updated from main branch

--- src/main/java/uk/co/softwarepulse/server/api/motivateme/data/Quote.java
+++ src/main/java/uk/co/softwarepulse/server/api/motivateme/data/Quote.java
@@ -1,5 +1,46 @@
 package uk.co.softwarepulse.server.api.motivateme.data;
 
 public class Quote {
+    private String author;
+    private String category;
+    private String quotation;
 
+    public Quote() {
+        this("", "", "");
+    }
+    
+    public Quote(String author, String category, String quotation) {
+        this.author = author;
+        this.category = category;
+        this.quotation = quotation;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCategory() {
+        return this.category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getQuotation() {
+        return this.quotation;
+    }
+
+    public void setQuotation(String quotation) {
+        this.quotation = quotation;
+    }
+
+    @Override
+    public String toString() {
+        return "Author: " + author + ", Category: " + category + ", Quote: " + quotation;
+    }
 }
