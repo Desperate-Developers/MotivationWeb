a8ce5314d0ff2676ceed036d13e69f248bdb7ec4
Klaus <Klausce1997@gmail.com>
2022-02-07 12:13:05 -0500

    Creating Method that queries from Database for an author.

--- src/main/java/uk/co/softwarepulse/server/api/motivateme/db/AuthorQuoteDAO.java
+++ src/main/java/uk/co/softwarepulse/server/api/motivateme/db/AuthorQuoteDAO.java
@@ -1,5 +1,49 @@
 package uk.co.softwarepulse.server.api.motivateme.db;
 
+import uk.co.softwarepulse.server.api.motivateme.data.Quote;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 public class AuthorQuoteDAO {
+
+    public List<Quote> getAuthor(String authorName)
+    {
+        List<Quote> listOfQuotes = new ArrayList<>() ;
+        String query = "SELECT * FROM motivate.quotations WHERE author="+authorName ;
+        String id, category, quotation, author ;
+
+        Connection connection ;
+        Statement statement ;
+        ResultSet resultSet ;
+        Quote quote ;
+
+        try {
+            connection = DatabaseAccess.createConnection();
+
+            statement = connection.createStatement() ;
+
+            resultSet = statement.executeQuery(query) ;
+
+            while(resultSet.next()) {
+                id = resultSet.getString("id") ;
+                author = resultSet.getString("author") ;
+                category = resultSet.getString("category") ;
+                quotation = resultSet.getString("quote") ;
+
+                quote = new Quote(id, author, category, quotation) ;
+
+                listOfQuotes.add(quote) ;
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace() ;
+        }
+
+        return listOfQuotes ;
+    }
 
 }
