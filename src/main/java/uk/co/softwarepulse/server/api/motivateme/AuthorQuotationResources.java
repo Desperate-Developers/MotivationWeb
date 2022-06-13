389e30920329f8abcd65000d6724b3bffcbfba19
Imran R <67403229+imran-2003@users.noreply.github.com>
2022-06-13 20:05:26 +0530

    Update AuthorQuotationResources.java

--- src/main/java/uk/co/softwarepulse/server/api/motivateme/AuthorQuotationResources.java
+++ src/main/java/uk/co/softwarepulse/server/api/motivateme/AuthorQuotationResources.java
@@ -1,5 +1,61 @@
 package uk.co.softwarepulse.server.api.motivateme;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.QueryParam;
+import uk.co.softwarepulse.server.api.motivateme.data.Quote;
+import uk.co.softwarepulse.server.api.motivateme.db.AuthorQuoteDAO;
+
+
+@Path("/quote/author")
 public class AuthorQuotationResources {
-//testing commit 3333
+
+    /**
+     * Used to obtain a single random quote by a particular author
+     * @param author name of the author sought
+     * @return Quote object
+     */
+    @GET
+    public Quote getQuote(@QueryParam("author") String author) {
+
+        AuthorQuoteDAO dao = new AuthorQuoteDAO() ;
+        List<Quote> quotes ;
+        Random r = new Random() ;
+
+        try {
+            quotes = dao.getAuthor(author) ;
+        }
+        catch (Exception e) {
+            return new Quote("-1", "ERROR", e.getCause().toString(), e.getLocalizedMessage()) ;
+        }
+
+        return quotes.get(r.nextInt(quotes.size())) ;
+    }
+
+
+    /**
+     * Used to obtain all the quotes by a particular author
+     * @param author name of the author sought
+     * @return List of Quote objects
+     */
+    @GET
+    @Path("/all")
+    public List<Quote> getAllQuotesBy(@QueryParam("author") String author) {
+
+        AuthorQuoteDAO dao = new AuthorQuoteDAO() ;
+        List<Quote> quotes = new ArrayList<>() ;
+
+        try {
+            quotes = dao.getAuthor(author) ;
+        }
+        catch (Exception e) {
+            quotes.add(new Quote("-1", "ERROR", e.getCause().toString(), e.getLocalizedMessage())) ;
+        }
+
+        return quotes ;
+    }
 }
