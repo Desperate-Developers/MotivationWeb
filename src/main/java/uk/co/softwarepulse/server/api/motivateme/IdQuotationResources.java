e18ef9e2643ca17c4d0b41a8c26741d0227b514c
Imran R <67403229+imran-2003@users.noreply.github.com>
2022-06-13 20:06:26 +0530

    Update IdQuotationResources.java

--- src/main/java/uk/co/softwarepulse/server/api/motivateme/IdQuotationResources.java
+++ src/main/java/uk/co/softwarepulse/server/api/motivateme/IdQuotationResources.java
@@ -1,14 +1,32 @@
-package uk.co.softwarepulse.server.api.motivateme;
+package uk.co.softwarepulse.server.api.motivateme ;
+
+import uk.co.softwarepulse.server.api.motivateme.data.Quote;
+import uk.co.softwarepulse.server.api.motivateme.db.IdQuoteDAO;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.QueryParam;
 
+
 @Path("/quote/id")
 public class IdQuotationResources {
 
+    /**
+     * Used to fetch a quote with a specific id from the database
+     * @param id the id sought
+     * @return a Quote object
+     */
     @GET
-    public String showQuoteBasedOnID(@QueryParam("id") int id) {
-        return "A quote is only as good as quote " + id + ".";
+    public Quote showQuoteBasedOnID(@QueryParam("id") String id) {
+        IdQuoteDAO quoteDAO = new IdQuoteDAO() ;
+        Quote quote;
+
+        try {
+            quote = quoteDAO.getId(id) ;
+        } catch (Exception e) {
+            quote = new Quote("-1", "ERROR", e.getCause().toString(), e.getLocalizedMessage()) ;
+        }
+
+        return quote ;
     }
 }
