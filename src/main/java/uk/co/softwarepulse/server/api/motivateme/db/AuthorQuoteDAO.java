7da6174fea6b8c6636850252f95986acd649d03a
Imran R <67403229+imran-2003@users.noreply.github.com>
2022-06-13 20:02:17 +0530

    Update AuthorQuoteDAO.java
    Altered the query to obtain data from the database.

--- src/main/java/uk/co/softwarepulse/server/api/motivateme/db/AuthorQuoteDAO.java
+++ src/main/java/uk/co/softwarepulse/server/api/motivateme/db/AuthorQuoteDAO.java
@@ -1,5 +1,53 @@
 package uk.co.softwarepulse.server.api.motivateme.db;
 
+import uk.co.softwarepulse.server.api.motivateme.data.Quote;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class AuthorQuoteDAO {
 
+    public List<Quote> getAuthor(String author) throws Exception
+    {
+        List<Quote> listOfQuotes = new ArrayList<>() ;
+        String id, category, quotation, query ;
+
+        Connection connection ;
+        Statement statement ;
+        ResultSet resultSet ;
+
+        DatabaseAccess da = new DatabaseAccess();
+        Quote quote = new Quote() ;
+
+        query = "SELECT * FROM motivate.quotations WHERE author LIKE '" + author + "%';" ;
+
+        connection = da.createConnection();
+
+        statement = connection.createStatement() ;
+
+        resultSet = statement.executeQuery(query) ;
+
+
+        if(!resultSet.next()) {
+            listOfQuotes.add(new Quote("-", "-", "-", "<<< no quotes by " + author + " >>>")) ;
+        }
+        else {
+            while (resultSet.next()) {
+                id = resultSet.getString("id");
+                author = resultSet.getString("author");
+                category = resultSet.getString("category");
+                quotation = resultSet.getString("quote");
+
+                quote = new Quote(id, author, category, quotation);
+
+                listOfQuotes.add(quote);
+            }
+        }
+
+        return listOfQuotes ;
+    }
 }
