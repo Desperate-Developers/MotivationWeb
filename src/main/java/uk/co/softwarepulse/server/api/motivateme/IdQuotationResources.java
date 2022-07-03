c7583005008215d5c33f272bd700c95da0ae96e6
Imran R <67403229+imran-2003@users.noreply.github.com>
2022-07-03 07:41:57 +0530

    Update IdQuotationResources.java

--- src/main/java/uk/co/softwarepulse/server/api/motivateme/IdQuotationResources.java
+++ src/main/java/uk/co/softwarepulse/server/api/motivateme/IdQuotationResources.java
@@ -3,9 +3,6 @@ package uk.co.softwarepulse.server.api.motivateme ;
 import uk.co.softwarepulse.server.api.motivateme.data.Quote;
 import uk.co.softwarepulse.server.api.motivateme.db.IdQuoteDAO;
 
-import uk.co.softwarepulse.server.api.motivateme.data.Quote;
-import uk.co.softwarepulse.server.api.motivateme.db.IdQuoteDAO;
-
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.QueryParam;
@@ -31,16 +28,5 @@ public class IdQuotationResources {
         }
 
         return quote ;
-
-        IdQuoteDAO quoteDAO = new IdQuoteDAO();
-        Quote quote;
-
-        try {
-            quote = quoteDAO.getId(id).get(0);
-        } catch (Exception e) {
-            quote = new Quote("-1", "", e.getCause().toString(), e.getLocalizedMessage());
-        }
-
-        return quote;
     }
 }
