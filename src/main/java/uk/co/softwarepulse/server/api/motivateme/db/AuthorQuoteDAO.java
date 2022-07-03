87abe63444638b640e30cc5573c6954fdca0b854
Imran R <67403229+imran-2003@users.noreply.github.com>
2022-07-03 07:43:38 +0530

    Update AuthorQuoteDAO.java

--- src/main/java/uk/co/softwarepulse/server/api/motivateme/db/AuthorQuoteDAO.java
+++ src/main/java/uk/co/softwarepulse/server/api/motivateme/db/AuthorQuoteDAO.java
@@ -8,20 +8,13 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class AuthorQuoteDAO {
 
     public List<Quote> getAuthor(String author) throws Exception
     {
         List<Quote> listOfQuotes = new ArrayList<>() ;
         String id, category, quotation, query ;
-
-public class AuthorQuoteDAO {
-
-    public List<Quote> getAuthor(String authorName)
-    {
-        List<Quote> listOfQuotes = new ArrayList<>() ;
-        String query = "SELECT * FROM motivate.quotations WHERE author="+authorName ;
-        String id, category, quotation, author ;
 
         Connection connection ;
         Statement statement ;
@@ -80,29 +73,6 @@ public class AuthorQuoteDAO {
         while (resultSet.next()) {
             author = resultSet.getString("author");
             listOfQuotes.add(author);
-
-        Quote quote ;
-        DatabaseAccess da = new DatabaseAccess();
-        try {
-            connection = da.createConnection();
-
-            statement = connection.createStatement() ;
-
-            resultSet = statement.executeQuery(query) ;
-
-            while(resultSet.next()) {
-                id = resultSet.getString("id") ;
-                author = resultSet.getString("author") ;
-                category = resultSet.getString("category") ;
-                quotation = resultSet.getString("quote") ;
-
-                quote = new Quote(id, author, category, quotation) ;
-
-                listOfQuotes.add(quote) ;
-            }
-        }
-        catch (Exception e) {
-            e.printStackTrace() ;
         }
 
         return listOfQuotes ;
